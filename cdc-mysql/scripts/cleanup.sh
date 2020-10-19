45541f90bfa3a47c80c0758ecc4e4631c2c1fcaa
ajhunyady <aj@fluvio.io>
2020-10-19 10:36:27 -0700

    udpated README.md file inside scripts directory

--- cdc-mysql/scripts/cleanup.sh
+++ cdc-mysql/scripts/cleanup.sh
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Clean-up with both profiles
-eval "$DIR/helpers/cleanup.sh  -f $DIR/../consumer_profile.toml"
 eval "$DIR/helpers/cleanup.sh  -f $DIR/../producer_profile.toml"
+eval "$DIR/helpers/cleanup.sh  -f $DIR/../consumer_profile.toml"
--- cdc-mysql/scripts/setup.sh
+++ cdc-mysql/scripts/setup.sh
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Runs setup with both profiles
-eval "$DIR//helpers/setup.sh  -f $DIR/../consumer_profile.toml"
 eval "$DIR//helpers/setup.sh  -f $DIR/../producer_profile.toml"
+eval "$DIR//helpers/setup.sh  -f $DIR/../consumer_profile.toml"
